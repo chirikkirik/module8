392ba72b02f3fdb9197f9b929b914a77e2fea7d7
chirikkirik <149368902+chirikkirik@users.noreply.github.com>
2024-10-06 17:30:52 +0300

    Update hw2.py

--- hw2.py
+++ hw2.py
@@ -1,23 +1,20 @@
 def personal_sum(numbers):
     result = 0
     incorrect_data = 0
-    try:
-        for i in numbers:
+    for i in numbers:
+        try:
             result += i
-    except TypeError  as exc:
-        incorrect_data += 1
+        except TypeError  as exc:
+            print(f"Некорректный тип данных для подсчёта суммы - {i}")
+            incorrect_data += 1
     return result, incorrect_data
 
 def calculate_average(numbers):
     try:
-        if isinstance(numbers, (list, tuple, set)):
-            result, incorrect_data = personal_sum(numbers)
-            if len(numbers) > 0:
-                return result / len(numbers)
-            else:
-                return 0
-        else:
-            raise TypeError
+        result, incorrect_data = personal_sum(numbers)
+        if len(numbers) == 0:
+            return 0
+        return result / len(numbers)
     except ZeroDivisionError:
         return 0
     except TypeError:
@@ -29,4 +26,5 @@ print(f'Результат 1: {calculate_average("1, 2, 3")}') # Строка п
 print(f'Результат 2: {calculate_average([1, "Строка", 3, "Ещё Строка"])}') # Учитываются только 1 и 3
 print(f'Результат 3: {calculate_average(567)}') # Передана не коллекция
 print(f'Результат 4: {calculate_average([42, 15, 36, 13])}') # Всё должно работать
+
 
